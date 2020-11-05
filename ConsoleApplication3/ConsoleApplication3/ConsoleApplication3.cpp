de13948739d398ce9d13491350eb02acdde9671f
KotletoVM <66645479+KotletoVM@users.noreply.github.com>
2020-11-05 03:06:24 +0300

    Чинил КР

--- ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
+++ ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
@@ -1,62 +1,59 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
 #include <string>
 #include <fstream>
 #include <windows.h>
 using namespace std::string_literals;
 using namespace std;
-class Zapis {
-public:
-    string lastname = "";
-    string firstname = "";
-    string pobatiushke;
-    string date = "";
-    int time = -1;
-    string polis;
-};
 class Raspisanie {
     public:
-        int i = 0;
-        Zapis* zapis = new Zapis[10];
+        int i = 0, j = 0;
         int formrasp() {
             fstream MF;
             MF.open("data.csv");
             string* datemas = new string[366];
+            string* timemas = new string[100];
             string firstname, lastname, svalka;
             while (MF.good()) {
-                    getline(MF, datemas[i], ',');
+                    getline(MF, datemas[i], ';');
                     getline(MF, svalka, '\n');
                     i++;
             }
             MF.close();
             MF.open("data.csv");
-            int a, s = 0, time;
-            int* zanyato = new int[10];
-            string date, ln, fn, pb, dat, vremya, imya, otch, fam, polis, zap = ",", per = "\n";
+            int a, s = 0, schet = 0;
+            bool n = true;
+            string date, ln, fn, pb, dat, vremya, imya, otch, fam, polis, zap = ";", per = "\n", time;
                 cout << "Укажите дату приема (xx.xx.xxxx):\n";
                 cin >> date;
-                for (int i = 0; i < 366; i++) {
-                    if (date == datemas[i]) {
-                        cout << "Занятое время для записи: \n";
+                cout << "Занятое время для записи: \n";
                         while (MF.good()){
-                            getline(MF, dat, ',');
+                            getline(MF, dat, ';');
+                            schet++;
                             if (dat == date) {
-                                getline(MF, vremya, ',');
-                                getline(MF, fam, ',');
-                                getline(MF, imya, ',');
-                                getline(MF, otch, ',');
+                                getline(MF, vremya, ';');
+                                getline(MF, fam, ';');
+                                getline(MF, imya, ';');
+                                getline(MF, otch, ';');
                                 getline(MF, polis, '\n');
                                 cout << vremya << ":00" << " " << fam << " " << imya << " " << otch << " " << polis << "\n";
+                                timemas[j] = vremya;
+                                j++;
+
                             }
-                            getline(MF, svalka, '\n');
+                            else {
+                                getline(MF, svalka, '\n');
+                            }
                         }
-                    }
-                    else {
-                    }
-                }
                 MF.close();
+                a:
                 cout << "Выберите время приема, отличное от занятого, в пределах от 9 до 17:";
                 cin >> time;
+                    for (int i = 0; i <= j; i++) {
+                        if (time == timemas[i]) {
+                            cout << "Вы выбрали занятое время, попробуйте еще раз:\n";
+                            goto a;
+                        }
+                    }
                 cout << "Укажите фамилию пациента\n";
                 cin >> ln;
                 cout << "Укажите имя пациента\n";
@@ -66,13 +63,14 @@ class Raspisanie {
                 cout << "Укажите номер полиса пациента\n";
                 cin >> polis;
                 MF.open("data.csv", std::ios::app);
-                MF << per << date << zap << time << zap << ln << zap << fn << zap << pb << zap << polis;
+                    MF << per << date << zap << time << zap << ln << zap << fn << zap << pb << zap << polis;
+                
                 MF.close();
                 
             return 0;
         };
         int coutraspdate() {
-            int n = 10;
+            int n = 0;
             string dat, vremya, fam, imya, otch, polis, svalka;
             string date;
             cout << "\n";
@@ -82,18 +80,22 @@ class Raspisanie {
             fstream MF;
             MF.open("data.csv");
             while (MF.good()) {
-                getline(MF, dat, ',');
+                getline(MF, dat, ';');
                 if (dat == date) {
-                    getline(MF, vremya, ',');
-                    getline(MF, fam, ',');
-                    getline(MF, imya, ',');
-                    getline(MF, otch, ',');
+                    getline(MF, vremya, ';');
+                    getline(MF, fam, ';');
+                    getline(MF, imya, ';');
+                    getline(MF, otch, ';');
                     getline(MF, polis, '\n');
                     cout << vremya << ":00" << " " << fam << " " << imya << " " << otch << " " << polis << "\n";
+                    n++;
                 }
                 else {
                     getline(MF, svalka, '\n');
                 }
+            }
+            if (n == 0) {
+                cout << "На выбранный день нет записей\n";
             }
             return 0;
         }
@@ -102,29 +104,30 @@ class Raspisanie {
             fstream MF;
             MF.open("data.csv");
             while (MF.good()) {
-                getline(MF, dat, ',');
-                getline(MF, vremya, ',');
-                getline(MF, fam, ',');
-                getline(MF, imya, ',');
-                getline(MF, otch, ',');
+                getline(MF, dat, ';');
+                getline(MF, vremya, ';');
+                getline(MF, fam, ';');
+                getline(MF, imya, ';');
+                getline(MF, otch, ';');
                 getline(MF, polis, '\n');
                 cout << dat << " " << vremya << ":00" << " " << fam << " " << imya << " " << otch << " " << polis << "\n";
                 }
+            MF.close();
             return 0;
             }
         int udal() {
-            string dat, vremya, fam, imya, otch, polis, zap = ",", per = "\n";
-            int i = 1, j = 0, nomer;
+            string dat, vremya, fam, imya, otch, polis, zap = ";", per = "\n";
+            int i = 1, j = 0, nomer, chisl = 0;
             fstream MF;
             MF.open("data.csv");
             fstream COPY;
             COPY.open("copy.csv");
             while (MF.good()) {
-                getline(MF, dat, ',');
-                getline(MF, vremya, ',');
-                getline(MF, fam, ',');
-                getline(MF, imya, ',');
-                getline(MF, otch, ',');
+                getline(MF, dat, ';');
+                getline(MF, vremya, ';');
+                getline(MF, fam, ';');
+                getline(MF, imya, ';');
+                getline(MF, otch, ';');
                 getline(MF, polis, '\n');
                 cout << i << ". " << dat << " " << vremya << ":00" << " " << fam << " " << imya << " " << otch << " " << polis << "\n";
                 i++;
@@ -135,14 +138,15 @@ class Raspisanie {
             cin >> nomer;
             i = 1;
             while (MF.good()) {
-                getline(MF, dat, ',');
-                getline(MF, vremya, ',');
-                getline(MF, fam, ',');
-                getline(MF, imya, ',');
-                getline(MF, otch, ',');
+                getline(MF, dat, ';');
+                getline(MF, vremya, ';');
+                getline(MF, fam, ';');
+                getline(MF, imya, ';');
+                getline(MF, otch, ';');
                 getline(MF, polis, '\n');
                 if (nomer == i) {
                     cout << "\nСтрока номер " << i << ". " << dat << " " << vremya << ":00" << " " << fam << " " << imya << " " << otch << " " << polis << " Удалена\n";
+                    chisl = 1;
                 }
                 else {
                     if (j == 0) {
@@ -151,15 +155,25 @@ class Raspisanie {
                     }
                     else {
                         COPY << per << dat << zap << vremya << zap << fam << zap << imya << zap << otch << zap << polis;
+
                     }
                 }
                 i++;
             }
             MF.close();
             COPY.close();
-            rename("data.csv", "copy1.csv");
-            rename("copy.csv", "data.csv");
-            rename("copy1.csv", "copy.csv");
+            if (chisl == 0) {
+                cout << "Строки с таким номером нет\n";
+            }
+            else if (chisl == 1){
+                rename("data.csv", "copy1.csv");
+                cout << "\n";
+                rename("copy.csv", "data.csv");
+                cout << "\n";
+                rename("copy1.csv", "copy.csv");
+                remove("copy.csv");
+                ofstream oFile("copy.csv");
+            }
             return 0;
         }
 };
@@ -170,9 +184,9 @@ int main()
     SetConsoleOutputCP(1251);
     setlocale(LC_ALL, "Russian");
     Raspisanie raspisanie;
-    bool n = true;
+    bool m = true;
     int deistv;
-    while (n)
+    while (m)
     {
         cout << "Выберите действие:\n";
         cout << "1. Добавить запись\n";
@@ -194,7 +208,7 @@ int main()
             raspisanie.udal();
         }
         else if (deistv == 5) {
-            n = false;
+            m = false;
         }
         else {
             cout << "Нет такого действия\n";
